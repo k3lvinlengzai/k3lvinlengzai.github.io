0c1be7261213887685e8599ceafa5b09ef9052ae
KELVIN\Leng Zai <k3lvinlkf@gmail.com>
2017-10-10 21:34:21 +0800

    new update

--- MadMan/Angular/controller.js
+++ MadMan/Angular/controller.js
@@ -11,8 +11,55 @@ app.controller('ContentController_News', function ($scope) {
         },
         {
             'img': 'https://i.pinimg.com/736x/ce/74/35/ce7435d514171d4947f2c73557bebcda--chrissy-constanza-hair-styles.jpg',
+            'title' : 'Game Spot',
+            'description' : 'this is the description',
+            'link' : '#',
+        },
+        {
+            'img': 'http://data.whicdn.com/images/147628795/original.jpg',
+            'title' : 'Gamerholic',
+            'description' : 'Game Studio',
+            'link' : '#',
+        },
+    ]
+});
+
+app.controller('ThumbnailController', function ($scope) {
+    $scope.thumbs = [
+        {
+            'img': 'http://data.whicdn.com/images/147628795/original.jpg',
             'title' : 'Square Enix',
             'description' : 'this is the description',
+            'link' : '#',
+        },
+        {
+            'img': 'http://data.whicdn.com/images/147628795/original.jpg',
+            'title' : 'Game Spot',
+            'description' : 'this is the description',
+            'link' : '#',
+        },
+        {
+            'img': 'http://data.whicdn.com/images/147628795/original.jpg',
+            'title' : 'Gamerholic',
+            'description' : 'Game Studio',
+            'link' : '#',
+        },
+        {
+            'img': 'http://data.whicdn.com/images/147628795/original.jpg',
+            'title' : 'Gamerholic',
+            'description' : 'Game Studio',
+            'link' : '#',
+        },
+        {
+            'img': 'http://data.whicdn.com/images/147628795/original.jpg',
+            'title' : 'Gamerholic',
+            'description' : 'Game Studio',
+            'link' : '#',
+        },
+        {
+            'img': 'http://data.whicdn.com/images/147628795/original.jpg',
+            'title' : 'Gamerholic',
+            'description' : 'Game Studio',
             'link' : '#',
         },
     ]
