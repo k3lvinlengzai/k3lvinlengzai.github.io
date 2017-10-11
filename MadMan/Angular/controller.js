0bfb89b50c3b786e6c45caf87c6dcd80917809e6
KELVIN\Leng Zai <k3lvinlkf@gmail.com>
2017-10-11 15:50:07 +0800

    Upcoming and reviews

--- MadMan/Angular/controller.js
+++ MadMan/Angular/controller.js
@@ -24,6 +24,53 @@ app.controller('ContentController_News', function ($scope) {
     ]
 });
 
+app.controller('SideBarController', function ($scope) {
+    $scope.upcoming = [
+        {
+            'id' : '1',
+            'img': 'http://data.whicdn.com/images/147628795/original.jpg',
+            'title' : 'Square Enix',
+            'description' : 'this is the description',
+            'link' : '#',
+        },
+        {
+            'id' : '2',
+            'img': 'https://i.pinimg.com/736x/ce/74/35/ce7435d514171d4947f2c73557bebcda--chrissy-constanza-hair-styles.jpg',
+            'title' : 'Game Spot',
+            'description' : 'this is the description',
+            'link' : '#',
+        },
+        {
+            'id' : '3',
+            'img': 'http://data.whicdn.com/images/147628795/original.jpg',
+            'title' : 'Gamerholic',
+            'description' : 'Game Studio',
+            'link' : '#',
+        },
+    ]
+    $scope.reviews = [
+        {
+            'id' : '1',
+            'img': 'http://data.whicdn.com/images/147628795/original.jpg',
+            'title' : 'Square Enix',
+            'description' : 'this is the description',
+            'link' : '#',
+            'rate' : 8.9,
+            'comment' : 'Good',
+        },
+        {
+            'id' : '2',
+            'img': 'http://data.whicdn.com/images/147628795/original.jpg',
+            'title' : 'Square Enix',
+            'description' : 'this is the description',
+            'link' : '#',
+            'rate' : 1,
+            'comment' : 'Sucks',
+        },
+    ]
+});
+
+
 app.controller('ThumbnailController', function ($scope) {
     $scope.thumbs = [
         {
